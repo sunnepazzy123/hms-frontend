df83f7425546203f5e2d354b99062b12f8aacd16
sunnepazzy123 <sunday.odibo@onoco.com>
2023-09-09 20:49:06 +0200

    update connfig

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,11 +1,8 @@
 import Patients from '@/components/pages/Patients'
 import React from 'react'
-// import { getServerSession} from 'next-auth'
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
 
 const PatientsPage = async () => {
-    //  const session = await getServerSession(authOptions)
-//  console.log(session, "patiiennts")
+
   return (
     <>
         <Patients />
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['img.icons8.com'], // Add the hostname(s) here
+      },
+      async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: '/api/:path*',
+          },
+        ];
+      },
+    //   async redirects() {
+    //     return [
+    //       {
+    //         source: '/patients',
+    //         destination: '/login',
+    //         permanent: false,
+    //       },
+    //     ];
+    // }
+}
 
 module.exports = nextConfig

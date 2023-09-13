074bf70879d8f1f5a0769f5ff6f92fb321b610f0
sunnepazzy123 <sunday.odibo@onoco.com>
2023-09-13 11:18:37 +0200

    update connfig

--- app/interfaces/patients.interface.ts
+++ app/interfaces/patients.interface.ts
@@ -1,6 +1,7 @@
 import { genderType, roleType} from "@/app/constant/index.constant";
 
 export interface IPatients {
+    _id?: string,
     firstName: string,
     lastName: string,
     gender: genderType,
